2ece5ba565dec33539b5dc9438e25980fa420fda
Sxnty <sxntycsgo@gmail.com>
2023-04-15 23:05:57 -0300

    configured electron and added webview

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+function App() {
+  return (
+    <>
+<webview src="https://www.google.com" style={{width: "100%", height: "100vh"}}></webview>
+
+    </>
+  );
+}
+
+export default App;
